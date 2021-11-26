4155594c30d8064693655211627894c395506026
John <john.mic.mckinnon@gmail.com>
2021-11-26 07:48:49 -0800

    Got the header component spec'd out, time to fill it in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 import MapContainer from "./common/map/containers/MapContainer";
 import SidebarContainer from "./common/sidebar/containers/SidebarContainer";
+import Header from "./common/header/Header"
 
 function App() {
   return (
-    <HStack bg="gray" height="100vh">
-      <SidebarContainer width="35%" />
-      <MapContainer />
-    </HStack>
+    <VStack align="stretch">
+      <Header/>
+      <HStack bg="gray" height="100vh">
+        <SidebarContainer width="35%" />
+        <MapContainer />
+      </HStack>
+      </VStack>
   );
 }
 
---
+++ src/common/header/Header.tsx
@@ -0,0 +1,12 @@
+import react from "react"
+import { HStack } from "@chakra-ui/react";
+
+function Header() {
+    return (
+        <HStack>
+           
+        </HStack>
+    )
+}
+
+export default Header
