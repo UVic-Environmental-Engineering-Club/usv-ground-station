7e94803f4f39ad0119a2e165454d7b74bd4109a4
John <john.mic.mckinnon@gmail.com>
2021-11-29 11:00:58 -0800

    minor clean up before PR

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,24 +1,20 @@
 import {Image, Flex, Button, Heading, HStack} from "@chakra-ui/react";
 
-//h=72px
 function Header() {
     return (
-    <HStack bg="white" h="72px" >
-        <Flex justify="space-between" w="100%" >
-            <HStack>
-                <Image src="https://github.com/UVic-Environmental-Engineering-Club/usv-ground-station/blob/main/public/logo.png?raw=true" alignSelf="center" m="5px 5px 5px 15px" width="50px" height="50px"/>
-                <Heading m="10px 15px" fontSize={18} > Uvic Environmental Engineering Club</Heading>
+        <HStack bg="white" h="72px" >
+            <Flex justify="space-between" w="100%" >
+                <HStack>
+                    <Image src="https://github.com/UVic-Environmental-Engineering-Club/usv-ground-station/blob/main/public/logo.png?raw=true" alignSelf="center" m="5px 5px 5px 15px" width="50px" height="50px"/>
+                    <Heading m="10px 15px" fontSize={18} > Uvic Environmental Engineering Club</Heading>
+                </HStack>
+                <HStack mr="15px">
+                    <Button m="auto" color="white" bg="#B71E39" h="48px" w="89px">Stop</Button>
+                    <Button m="auto" color="white" bg="#319795" h="48px" w="89px">Start</Button>
             </HStack>
-            <HStack mr="15px">
-                <Button m="auto" color="white" bg="#B71E39" h="48px" w="89px">Stop</Button>
-                <Button m="auto" color="white" bg="#319795" h="48px" w="89px">Start</Button>
-           </HStack>
-        </Flex>
-    </HStack>
-
-   
+            </Flex>
+        </HStack>
     )
 }
 
 export default Header
-
