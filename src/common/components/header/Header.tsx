e98b2af466e4543628c5a975e19ba5bfe23dc014
John <john.mic.mckinnon@gmail.com>
2021-11-29 13:25:08 -0800

    minor changes

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -4,10 +4,10 @@ function Header() {
     return (
         <Flex bg="white" h="8%" justify="space-between" w="100%" >
             <HStack alignItems="center" mx="15px">
-                <Image src="https://github.com/UVic-Environmental-Engineering-Club/usv-ground-station/blob/main/public/logo.png?raw=true" alignSelf="center" m="5px" width="50px" height="50px"/>
+                <Image src="https://github.com/UVic-Environmental-Engineering-Club/usv-ground-station/blob/main/public/logo.png?raw=true" my="5px" w="50px" h="50px"/>
                 <Heading as='h1' size="md" isTruncated py="5px"> Uvic Environmental Engineering Club</Heading>
             </HStack>
-            <HStack mr="15px" alignItems="center">
+            <HStack mx="15px" alignItems="center">
                 <ButtonGroup mx="10px" spacing="20px"size="lg">
                     <Button color="white" bg="#B71E39">Stop</Button>
                     <Button color="white" bg="#319795">Start</Button>
