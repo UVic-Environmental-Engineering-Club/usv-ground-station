3ec0b31868ef8d6b1ebb09f7e0458be849b88ed7
Alex <zthh626@gmail.com>
2021-12-08 12:37:12 -0800

    Add StatusbarContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { HStack } from "@chakra-ui/react";
+import { Flex, HStack } from "@chakra-ui/react";
 import MapContainer from "./common/map/containers/MapContainer";
 import SidebarContainer from "./common/sidebar/containers/SidebarContainer";
+import StatusbarContainer from "./common/statusbar/containers/StatusbarContainer";
 
 function App() {
   return (
     <HStack bg="gray" height="100vh">
       <SidebarContainer width="35%" />
-      <MapContainer />
+      <Flex flexDir="column" w="100%" h="100%">
+        <MapContainer />
+        <StatusbarContainer />
+      </Flex>
     </HStack>
   );
 }
---
+++ src/common/statusbar/containers/StatusbarContainer.tsx
@@ -0,0 +1,8 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+function StatusbarContainer() {
+  return <Flex grow={1} h="20%" bg="yellow" />;
+}
+
+export default StatusbarContainer;
