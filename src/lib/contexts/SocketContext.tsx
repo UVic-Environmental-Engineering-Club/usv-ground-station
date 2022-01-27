9d2fcbb1942c45430bfe38105ca9ddb9c446502d
Alex <zthh626@gmail.com>
2022-01-27 10:54:34 -0800

    fix: socket not cleaning up

--- src/lib/contexts/SocketContext.tsx
+++ src/lib/contexts/SocketContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, PropsWithChildren, useState } from "react";
-import { socket } from "../SocketManager";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 export interface SocketContext {
   logs: string[];
@@ -10,9 +10,20 @@ export const Context = createContext<SocketContext>({ logs: [] });
 export function SocketProvider({ children }: PropsWithChildren<any>) {
   const [logs, setLogs] = useState<string[]>([]);
 
-  socket.on("log", (log) => {
-    setLogs((prev) => [...prev, log]);
-  });
+  useEffect(() => {
+    const socket = io("ws://localhost:8080/groundstation");
+
+    socket.on("connect", () => {
+      console.log("connected to groundstation");
+    });
+    socket.on("log", (log) => {
+      setLogs((prev) => [...prev, log]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return <Context.Provider value={{ logs: logs }}>{children}</Context.Provider>;
 }
